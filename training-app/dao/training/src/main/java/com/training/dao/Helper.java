3dbb0e262ddf3dc7249c82201f516d5f8f979eee
KantorSV <kantorsergei87@gmail.com>
2023-07-31 17:47:41 +0300

    Commit

--- training-app/dao/training/src/main/java/com/training/dao/Helper.java
+++ training-app/dao/training/src/main/java/com/training/dao/Helper.java
@@ -5,7 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
+
+
 public class Helper {
+
+
+    String t;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/training", "root", "11111111");
